9fce1688991673a71793f45bdb6116d75da3f661
Tim Sonnen <tsonnen67@gmail.com>
2019-02-03 18:14:23 -0800

    Add in widgets for add/show character

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
             bottom: TabBar(
@@ -25,12 +25,54 @@ class MyApp extends StatelessWidget {
           ),
           body: TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
+              showCharacters(),
+              addCharacter(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // TODO: Implement showing the character
+  Widget showCharacters(){
+    return Scaffold(
+      body: Text("Super Dope"),
+    );
+  }
+
+  //TODO: Implement adding a charcter
+  Widget addCharacter(){
+    return Scaffold(
+      body: Text("Super Dope"),
+    );
+  }
+}
+
+class ShowActorsWidget extends StatefulWidget{
+  @override
+  ShowActorsApp createState () => new ShowActorsApp();
+}
+
+class ShowActorsApp extends State<ShowActorsWidget>{
+  int counter = 0;
+  bool cboxValue = false;
+  
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Column(
+      children: <Widget>[
+          new Text("count = $counter"),
+          new Checkbox(
+              value: cboxValue,
+              onChanged: (bool newValue) {
+                setState((){
+                  cboxValue = newValue;
+                  counter++;
+                });
+              }
+          )
+      ],
     );
   }
 }
