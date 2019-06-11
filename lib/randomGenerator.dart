84cf3e859046605349f8103f1095515de160dd6b
Tim Sonnen <tsonnen67@gmail.com>
2019-06-11 01:12:11 -0700

    Update to not allow rolling a "0"

--- lib/randomGenerator.dart
+++ lib/randomGenerator.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 int rollDice(int amount, int sides){
   if(amount > 0){
-    return Random().nextInt(sides) + rollDice(amount - 1, sides);
+    return 1 + Random().nextInt(sides - 1) + rollDice(amount - 1, sides);
   }
   return 0;
 }
