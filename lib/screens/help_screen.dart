e29b5a5a8a9ee323cdf6fcecf9829d5c485dd332
tobye <tsonnen67@gmail.com>
2019-06-23 09:48:32 -0700

    Add in navigation arrows

--- lib/screens/help_screen.dart
+++ lib/screens/help_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
@@ -9,42 +10,78 @@ class HelpPage extends StatefulWidget {
   HelpPageState createState() => HelpPageState();
 }
 
-//TODO: Add in navigation buttons to allow users to go back to their previous page
 class HelpPageState extends State<HelpPage> {
-
   String markdown = '';
+  String _currentPage;
+  Queue backStack = new Queue();
+  Queue forwardStack = new Queue();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-
+  
     showHelp("table_contents");
   }
 
   @override
   Widget build(BuildContext context) {
+
     return new Scaffold(
       appBar: AppBar(
         title: Text('Help'),
       ),
       body: new Markdown(
         data: markdown,
-        onTapLink: (value){
+        onTapLink: (value) {
+          backStack.addLast(_currentPage);
+          forwardStack.clear();
           showHelp(value);
         },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.navigate_before),
+                onPressed: () {
+                  if (backStack.isNotEmpty){
+                    forwardStack.addLast(_currentPage);
+                    showHelp(backStack.removeLast());
+                  }
+                },
+                color: backStack.isNotEmpty
+                    ? Theme.of(context).buttonColor
+                    : Theme.of(context).disabledColor,
+              ),
+              IconButton(
+                icon: Icon(Icons.navigate_next),
+                onPressed: () {
+                  if (forwardStack.isNotEmpty){
+                    backStack.addLast(_currentPage);
+                    showHelp(forwardStack.removeLast());
+                  }
+                },
+                color: forwardStack.isNotEmpty
+                    ? Theme.of(context).buttonColor
+                    : Theme.of(context).disabledColor,
+              ),
+            ]),
       ),
     );
   }
 
-  void showHelp(String file){
-    loadHelpFile(file).then((value){
+  showHelp(String file) {
+    loadHelpFile(file).then((value) {
       setState(() {
         markdown = value;
+        _currentPage = file;
       });
     });
   }
 
   Future<String> loadHelpFile(String file) async {
-    return await rootBundle.loadString('assets/helpx/' + file + '.md');
+    return await rootBundle.loadString('assets/help/' + file + '.md');
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:initiative_tracker/character_model.dart';
@@ -29,68 +28,67 @@ class HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return new ScopedModelDescendant<CharacterListModel>(
       builder: (context, child, model) => Scaffold(
-            appBar: AppBar(
-              title: Text(titleText),
-              actions: <Widget>[
-                //Add the dropdown widget to the `Action` part of our appBar. it can also be among the `leading` part
+        appBar: AppBar(
+          title: Text(titleText),
+          actions: <Widget>[
+            //Add the dropdown widget to the `Action` part of our appBar. it can also be among the `leading` part
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                model.clear();
+                updateTitle(model);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => SettingsPage()));
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.help_outline),
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => HelpPage()));
+              },
+            )
+          ],
+        ),
+        body: Container(
+          child: createCharacterList(),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AddCharacterPage()));
+          },
+          tooltip: 'Add Character',
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: new Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
                 IconButton(
-                  icon: Icon(Icons.refresh),
+                  icon: Icon(Icons.navigate_before),
                   onPressed: () {
-                    model.clear();
+                    model.prevRound();
                     updateTitle(model);
                   },
                 ),
                 IconButton(
-                  icon: Icon(Icons.settings),
+                  icon: Icon(Icons.navigate_next),
                   onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SettingsPage()));
+                    model.nextRound();
+                    updateTitle(model);
                   },
                 ),
-                IconButton(
-                  icon: Icon(Icons.help_outline),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => HelpPage()));
-                  },
-                )
-              ],
-            ),
-            body: Container(
-              child: createCharacterList(),
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerDocked,
-            floatingActionButton: FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => AddCharacterPage()));
-              },
-              tooltip: 'Add Character',
-            ),
-            bottomNavigationBar: BottomAppBar(
-              child: new Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.navigate_before),
-                      onPressed: () {
-                        model.prevRound();
-                        updateTitle(model);
-                      },
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.navigate_next),
-                      onPressed: () {
-                        model.nextRound();
-                        updateTitle(model);
-                      },
-                    ),
-                  ]),
-            ),
-          ),
+              ]),
+        ),
+      ),
     );
   }
 
@@ -100,47 +98,48 @@ class HomeScreenState extends State<HomeScreen> {
             children: model.characters
                 .map(
                   (item) => Card(
-                        child: ListTile(
-                          title: new Text(item.name),
-                          isThreeLine: true,
-                          subtitle: new Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              new IconButton(
-                                icon: new Icon(Icons.remove),
-                                color: Colors.red,
-                                onPressed: () {
-                                  model.reduceHP(item);
-                                },
-                              ),
-                              new Text(
-                                item.hp.toString(),
-                                textAlign: TextAlign.right,
-                                style: TextStyle(
-                                    color: item.hp < 0
-                                        ? Colors.red
-                                        : DynamicTheme.of(context).data.textTheme.body1.color),
-                              ),
-                              new IconButton(
-                                icon: new Icon(Icons.add),
-                                onPressed: () {
-                                  model.increaseHP(item);
-                                },
-                                color: Colors.green,
-                              )
-                            ],
+                    child: ListTile(
+                      title: new Text(item.name),
+                      isThreeLine: true,
+                      subtitle: new Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new IconButton(
+                            icon: new Icon(Icons.remove),
+                            color: Colors.red,
+                            onPressed: () {
+                              model.reduceHP(item);
+                            },
                           ),
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) =>
-                                    EditCharacterPage(item: item)));
-                          },
-                          onLongPress: () {
-                            model.removeCharacter(item);
-                          },
-                        ),
+                          new Text(
+                            item.hp.toString(),
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: item.hp < 0
+                                  ? Colors.red
+                                  : Theme.of(context).textTheme.body1.color,
+                            ),
+                          ),
+                          new IconButton(
+                            icon: new Icon(Icons.add),
+                            onPressed: () {
+                              model.increaseHP(item);
+                            },
+                            color: Colors.green,
+                          )
+                        ],
                       ),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                EditCharacterPage(item: item)));
+                      },
+                      onLongPress: () {
+                        model.removeCharacter(item);
+                      },
+                    ),
+                  ),
                 )
                 .toList()));
   }
