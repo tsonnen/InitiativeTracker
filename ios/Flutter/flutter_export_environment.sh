4cd3250ef52b4e265a500f4a209b7933a8f1f45e
tsonnen <tsonnen67@gmail.com>
2021-02-09 23:18:03 -0800

    Change Travis to stable branch

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\Tools\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\code\InitiativeTracker"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/home/tobye/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/tobye/code/initiative_tracker"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=E:\Tools\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/tobye/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.1.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
