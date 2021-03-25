2a3a6f164bb87d4013dde123345468011ced40bd
tsonnen <tsonnen67@gmail.com>
2021-03-24 23:22:57 -0700

    Fix for outdated sqlite version

--- lib/moor/database.dart
+++ lib/moor/database.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:initiative_tracker/moor/parties_dao.dart';
 import 'package:initiative_tracker/helpers/uuid.dart';
 import 'package:moor/moor.dart';
+import 'package:moor/ffi.dart';
 import 'package:moor_flutter/moor_flutter.dart';
 import 'package:initiative_tracker/moor/parties.dart';
 import 'package:initiative_tracker/models/character_list.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 part 'database.g.dart';
 
@@ -13,9 +18,13 @@ part 'database.g.dart';
 class Database extends _$Database {
   // we tell the database where to store the data with this constructor
   Database()
-      : super(FlutterQueryExecutor.inDatabaseFolder(
-          path: 'db.sqlite',
-        ));
+      : super(
+          LazyDatabase(() async {
+            final dbFolder = await getDatabasesPath();
+            final file = File(join(dbFolder, 'db.sqlite'));
+            return VmDatabase(file);
+          }),
+        );
 
   // you should bump this number whenever you change or add a table definition. Migrations
   // are covered later in this readme.
