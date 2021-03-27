978bdc8f732685798b05f96c2a31b7062393b898
tsonnen <tsonnen67@gmail.com>
2021-03-26 22:32:41 -0700

    Start adding bloc tests

---
+++ test/bloc_test/party_bloc_test.dart
@@ -0,0 +1,16 @@
+import 'package:initiative_tracker/bloc/party/party_bloc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Party Bloc', () {
+    var partyBloc;
+
+    setUp(() {
+      partyBloc = PartyBloc();
+    });
+
+    test('initial state should be PartyInitial', () async {
+      await expectLater(partyBloc.state is PartyInitial, true);
+    });
+  });
+}
